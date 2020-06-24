28e0938dd23be2c3643a9709f3d7b6a393c36f91
akhiljayan29aj <58233202+akhiljayan29aj@users.noreply.github.com>
2020-06-25 00:57:07 +0530

    News extractor done

--- public/python/newsExtractor.py
+++ public/python/newsExtractor.py
@@ -1,23 +1,28 @@
+# Importing Libraries
 from newsapi import NewsApiClient
 import pandas as pd
 
+# Setting up client using API Key
 newsapi = NewsApiClient(api_key='eefe7e300a174e9f8ca25f2ad39cde63')
+# Extracting top headlines
 top_headlines = newsapi.get_top_headlines(q='covid',language='en',country='in')
+# Extracting articles from the headlines
 art = top_headlines["articles"]
 
+# Setting up variables
 titl=[]
 links=[]
 img=[]
 
+# appending required details in the above variables
 for i in range(3):
     titl.append(art[i]["title"].replace(',',''))
-
-for i in range(3):
     links.append(art[i]["url"].replace(',',''))
-
-for i in range(3):
     img.append(art[i]["urlToImage"].replace(',',''))
 
+# Making a dictionary
 dct = {"titles":titl,"links":links,"images":img}
+# converting the dictionary into DataFrame
 df = pd.DataFrame(dct)
+# Converting the DataFrame into CSV File
 df.to_csv('../data/news.csv')
